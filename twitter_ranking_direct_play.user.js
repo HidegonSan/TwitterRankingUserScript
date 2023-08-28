e7d3eacfe5541096e48ddccf56fb76774c088e8a
Hidegon <68275072+HidegonSan@users.noreply.github.com>
2023-08-28 13:53:18 +0900

    Update twitter_ranking_direct_play.user.js

--- twitter_ranking_direct_play.user.js
+++ twitter_ranking_direct_play.user.js
@@ -1,12 +1,20 @@
 // ==UserScript==
 // @name         Twitter保存ランキング 直接再生
-// @version      0.0.1
-// @description  各種Twitter保存ランキングで動画を直接再生できるようにします (twihub.me, twidouga.net, twivideo.net)
+// @version      0.0.7
+// @description  各種Twitter保存ランキングで動画を直接再生できるようにします (twihub.me, twidouga.net, twivideo.net, twiigle.com, twicoco.com, twidou.link)
 // @match        *://twihub.me/v2/detail.php*
 // @match        *://www.twidouga.net/*
 // @match        *://twivideo.net/*
+// @match        *://twiigle.com/*
+// @match        *://twicoco.com/*
+// @match        *://www.twidou.link/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
 // @grant        none
+// @author       Hidegon
+// @updateURL    https://raw.githubusercontent.com/HidegonSan/TwitterRankingUserScript/main/twitter_ranking_direct_play.user.js
+// @downloadURL  https://raw.githubusercontent.com/HidegonSan/TwitterRankingUserScript/main/twitter_ranking_direct_play.user.js
+// @supportURL   https://github.com/HidegonSan/TwitterRankingUserScript/issues
+// @homepage     https://github.com/HidegonSan/TwitterRankingUserScript
 // ==/UserScript==
 
 // License: WTFPL
@@ -15,11 +23,16 @@
 try {
 
 
+function get_extension(url) {
+  return url.split(".").slice(-1)[0].split("?")[0]
+}
+
+
 // twihub.me
 if (location.origin == "https://twihub.me") {
   var card = document.getElementsByClassName("c_detail-card")[0]
   var video_url = document.querySelectorAll("a[href^='https://video.twimg.com/']")[0].href
-  var video_extension = video_url.split(".").slice(-1)[0].split("?")[0]
+  var video_extension = get_extension(video_url)
   var element = `<video controls=""><source src="${video_url}" type="video/${video_extension}"></video>`
   card.childNodes.forEach(e => { e.remove() })
   card.insertAdjacentHTML("afterbegin", element)
@@ -27,16 +40,18 @@ if (location.origin == "https://twihub.me") {
   card.childNodes[0].style.cssText += "max-height: 80vh;"
 } // End of twihub.me
 
+
 // twidouga.net
 else if (location.origin == "https://www.twidouga.net") {
   document.querySelectorAll(".gazou > .poster > a[href^='https://video.twimg.com/']").forEach(function(e) {
     var video_url = e.href
-    var video_extension = video_url.split(".").slice(-1)[0].split("?")[0]
+    var video_extension = get_extension(video_url)
     var element = `<video controls="" style="width: 300px; border-radius: 15px;"><source src="${video_url}" type="video/${video_extension}"></video><br>`
     e.parentNode.parentNode.insertAdjacentHTML("afterend", element)
     e.parentNode.parentNode.remove()
   })
 } // End of twidouga.net
+
 
 // twivideo.net
 else if (location.origin == "https://twivideo.net") {
@@ -57,8 +72,8 @@ else if (location.origin == "https://twivideo.net") {
       if (start <= i && i <= end) {
         var video = videos[i]
         var video_url = video.childNodes[1].href
-        var video_extension = video_url.split(".").slice(-1)[0].split("?")[0]
-        var element = `<a href="${video_url}" target="_blank"><video controls="" style="width: 100%; height: auto; object-fit: contain; border-radius: 3px;" preload="metadata"><source src="${video_url}#t=0.1" type="video/${video_extension}"></video></a><br>`
+        var video_extension = get_extension(video_url)
+        var element = `<video controls="" style="width: 100%; height: auto; object-fit: contain; border-radius: 3px;" preload="metadata"><source src="${video_url}#t=0.1" type="video/${video_extension}"></video><br>`
         video.insertAdjacentHTML("afterbegin", element)
         video.childNodes[3].remove()
       }
@@ -68,15 +83,15 @@ else if (location.origin == "https://twivideo.net") {
   function patched_show_more() {
 	  var video_counts = document.getElementsByClassName("art_li").length
     replace_to_video(45*show_more_called, 45*(show_more_called + 1) - 1)
-    setInterval(
-      function() {
-        if ($) {
+    if ($) {
+      setInterval(
+        function() {
           $(".grids").masonry("reloadItems")
           $(".grids").masonry('layout')
-        }
-      },
-      500
-    )
+        },
+        500
+      )
+    }
 	  if (video_counts < 1000 || video_counts < 0) {
       document.getElementById("read_page").style.display = "block"
       show_more_called += 1;
@@ -90,6 +105,45 @@ else if (location.origin == "https://twivideo.net") {
   show_more = patched_show_more;
 
 } // End of twivideo.net
+
+
+// twiigle.com
+else if (location.origin == "https://twiigle.com") {
+  document.querySelectorAll(".item_image").forEach(function(video) {
+    var video_url = video.childNodes[1].href
+    var video_extension = get_extension(video_url)
+    var element = `<video controls="" style="width: 100%; height: auto; object-fit: contain; border-radius: 3px;" preload="metadata"><source src="${video_url}#t=0.1" type="video/${video_extension}"></video><br>`
+    video.insertAdjacentHTML("afterbegin", element)
+    video.childNodes[3].remove()
+  })
+  if ($) {
+    setInterval(
+      function() {
+        $(".grids").imagesLoaded(function() {
+          $(".grids").masonry({
+            itemSelector: ".art_li",
+            gutterWidth: 0,
+            isAnimated: true,
+            isResizable: true
+          });
+        });
+      },
+      500
+    )
+  }
+} // End of twiigle.com
+
+
+// twicoco.com, www.twidou.link
+else if (location.origin == "https://twicoco.com" || location.origin == "https://www.twidou.link") {
+  document.querySelectorAll(".video-card").forEach(function(card) {
+    var video_url = card.parentNode.href
+    var video_extension = get_extension(video_url)
+    var element = `<video controls="" style="width: 100%;" preload="metadata"><source src="${video_url}#t=0.1" type="video/${video_extension}"></video><br>`
+    card.parentNode.insertAdjacentHTML("afterend", element)
+    card.remove()
+  })
+} // End of twicoco.com, www.twidou.link
 
 
 } catch (e) { }
